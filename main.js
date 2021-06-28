740f746e6425e63d691b38fcf1e8ca86f4991c59
ARTHUR FINCHAM <arthurfincham@me.com>
2021-06-28 18:43:11 +0100

    logic working

--- main.js
+++ main.js
@@ -10,12 +10,28 @@ slider.oninput = function() {
 
 function myFunction() {
 
-  var decade = document.getElementById("year").innerHTML;
-  var scorer = document.querySelector('input[name="scorer"]:checked').value
-  var champs = document.getElementById('location').value;
+  var decade = document.getElementById("year").innerHTML; 
+  var scorer = document.querySelector('input[name="scorer"]:checked').value 
+  var champs = document.getElementById('location').value; 
   var mascot = document.querySelector('input[name="mascot"]:checked').value
+  var array = []
+  var checkboxes = document.querySelectorAll('input[type=checkbox]:checked')
+  
+  for (var i = 0; i < checkboxes.length; i++) {
+    array.push(checkboxes[i].value)
+  }
 
-  document.getElementById("answers").innerHTML = "1870 --  " + decade + "<br>" + "Lampard --  " + scorer + "<br>" + "munich --  " + champs + "<br>" + "Stamford the Lion --  " + mascot
+  var nation = array.reduce((a,b) => a + b)
+
+  let score = 0
+
+  decade == 1870 ? score += 1 : score += 0
+  scorer == 1 ? score += 1 : score += 0
+  champs == "munich" ? score += 1 : score += 0
+  mascot == 1 ? score += 1 : score += 0
+  nation == 11 ? score += 1 : score += 0
+
+  document.getElementById("answers").innerHTML = "You got " + score + " out of five!"
 } 
 
 // Anchor Link Smooth Scrolling
