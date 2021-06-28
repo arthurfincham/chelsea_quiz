59ba5fdf62ad6a421b23d4ac9d367c6e707716ac
ARTHUR FINCHAM <arthurfincham@me.com>
2021-06-28 17:28:16 +0100

    update

--- main.js
+++ main.js
@@ -17,3 +17,25 @@ function myFunction() {
 
   document.getElementById("answers").innerHTML = "1870 --  " + decade + "<br>" + "Lampard --  " + scorer + "<br>" + "munich --  " + champs + "<br>" + "Stamford the Lion --  " + mascot
 } 
+
+// Anchor Link Smooth Scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
+
+// Nationality checkbox limiter
+var checks = document.querySelectorAll(".nationman");
+var max = 2;
+for (var i = 0; i < checks.length; i++)
+  checks[i].onclick = selectiveCheck;
+function selectiveCheck (event) {
+  var checkedChecks = document.querySelectorAll(".nationman:checked");
+  if (checkedChecks.length >= max + 1)
+    return false;
+}
